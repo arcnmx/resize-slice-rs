f172d10387be1e69707d1f5de90205708689114f
ghost <ghost@konpa.ku>
2018-05-13 01:38:55 +0000

    0.1.3 - a7f0968d405322df05b6e28ebedfe883eafda549

--- implementors/resize_slice/trait.SliceExt.js
+++ implementors/resize_slice/trait.SliceExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['resize_slice'] = [];
+implementors["resize_slice"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
